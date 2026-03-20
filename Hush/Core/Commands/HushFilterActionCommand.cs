3d4e98d798cff80fc2483e41aff80fd73070b272
AndrewLin <andrewlimforfun@gmail.com>
2026-03-20 23:18:48 +0100

    hush filter action command

---
+++ Hush/Core/Commands/HushFilterActionCommand.cs
@@ -0,0 +1,44 @@
+﻿using Hush;
+using Alpha.Core.Util;
+using Alpha.Core.Command;
+using Hush.Core;
+using System;
+
+namespace Hush.Core.Commands
+{
+    public class HushFilterActionCommand : IChatCommand
+    {
+        public const string CMD = "hushfilteraction";
+        public string Name => CMD;
+        public string ShortName => "hfa";
+        public string Description => "Set Hush filter action command: /hushfilteraction <action>. Valid actions: block, censor.";
+
+        public string Namespace => "hush";
+        public void Execute(string[] args)
+        {
+            if (HushPlugin.FilterActionConfig == null)
+            {
+                return;
+            }
+
+            if (args.Length == 0)
+            {
+                ChatUtils.AddGlobalNotification("Usage: /hushfilteraction <action>. Valid actions: block, censor.");
+                return;
+            }
+
+            if (Enum.TryParse(args[0], ignoreCase: true, out FilterAction action))
+            {
+                HushPlugin.FilterActionConfig.Value = action;
+                ChatUtils.AddGlobalNotification($"Hush filter action set to {action}.");
+            }
+            else
+            {
+                ChatUtils.AddGlobalNotification("Invalid action. Valid actions: block, censor.");
+            }
+
+            // HushPlugin.FilterAction.Value = ;
+            // ChatUtils.AddGlobalNotification($"Hush feature is now {(HushPlugin.EnableFeature.Value ? "enabled" : "disabled")}.");
+        }
+    }
+}
